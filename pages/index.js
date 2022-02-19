49feb1e656065584cdbda03a9b5f78a2b8bb22db
Behailu Tekletsadik <behailutekletsadik@Behailus-MacBook-Pro.local>
2022-02-19 10:07:13 -0500

    a bunch of shit

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,17 @@ export default function Home() {
     <div className="">
       <Head>
         <title>Archetype</title>
+        <meta name="twitter:card" content="Updates, memes and all things APIs and programming from Archetype" key="twcard" />
+        <meta name="twitter:creator" content="GetArchetype" key="twhandle" />
+
+        <meta property="og:url" content="https://archetype.dev" key="ogurl" />
+        <meta property="og:site_name" content="Archetype" key="ogsitename" />
+        <meta property="og:title" content="Archetype - API Monetization"key="ogtitle" />
+        <meta property="og:description" content="Helping APIs launch monetization in minutes. Quickly create subscriptions, gate endpoint access, manage users' quotas and set up usage based pricing and optimize your pricing models with our end-to-end API analytics and logs." key="ogdesc" />
+
         <link rel="icon" href="/favicon.ico" />
         <link href="/dist/output.css" rel="stylesheet"></link>
+        <meta name="description" content="Helping APIs launch monetization in minutes. Quickly create subscriptions, gate endpoint access, manage users' quotas and set up usage based pricing and optimize your pricing models with our end-to-end API analytics and logs." />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-8PE8JSX9VB"
