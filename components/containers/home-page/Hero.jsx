18399ac95fd809749b7cadaf2d4d9c247d0e762a
ahmedabdikani <ahmed_abdikani@hotmail.com>
2022-06-12 14:53:44 +0800

    fix hero text size

--- components/containers/home-page/Hero.jsx
+++ components/containers/home-page/Hero.jsx
@@ -9,8 +9,8 @@ import { Body, LargeHeading } from "@components/shared/typography";
 const Hero = () => {
   return (
     <Container id="main" className="mt-0 lg:mt-14 overflow-hidden relative">
-      <HStack className="lg:items-end w-full justify-between  flex-col-reverse lg:flex-row">
-        <VStack className="w-[242px]  md:w-[420px] gap-6 md:gap-6 xl:gap-10 -translate-y-1/4">
+      <HStack className="lg:items-end w-full justify-between lg:gap-6 flex-col-reverse lg:flex-row">
+        <VStack className="w-[242px]  md:w-[420px] lg:w-[600px] gap-6 md:gap-6 xl:gap-10 -translate-y-1/4 lg:translate-y-0">
           <LargeHeading>Effortless Usage-based billing for APIs</LargeHeading>
           <Body className="">
             Archetype helps API companies launch, iterate and scale their
